84a07a19b956f123ed585e6ea8fc12fe3b4b5469
landrozz <leandrozanobine@gmail.com>
2023-01-27 20:26:47 -0300

    queries criadas

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -21,3 +21,35 @@ SELECT * FROM products;
 
 INSERT INTO products (id, name, price, category)
 VALUES (6, "Fone de ouvido sem fio Pulse PS5", 600, "Periferico");
+
+SELECT * FROM products
+WHERE name LIKE "%tendo%";
+
+SELECT * FROM products
+WHERE id = 6;
+
+DELETE FROM users
+WHERE id = 4;
+
+DELETE FROM products
+WHERE id = 6;
+
+UPDATE users
+SET password = "SENHAMELHOR"
+WHERE id = 1;
+
+UPDATE products
+SET price = 150
+WHERE id = 1;
+
+SELECT * FROM users
+ORDER BY email ASC;
+
+SELECT * FROM products
+ORDER BY price ASC
+LIMIT 20;
+
+SELECT * FROM products
+WHERE price >= 100
+  AND price <= 300
+ORDER BY price ASC;
